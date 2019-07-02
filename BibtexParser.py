dd7e330fcf19656f711350c0817f6d7a2f33ad6f
JP <jonas.parchent@gmail.com>
2019-07-02 23:18:08 +0200

    added functions to search for entries

--- BibtexParser.py
+++ BibtexParser.py
@@ -4,15 +4,15 @@ from BibtexEntry import BibtexEntry
 
 
 class BibtexParser:
-    def __init__(self):
-        self.entries = []
+    def __init__(self, entries=None):
+        self.entries = [] if entries is None else entries
         # maybe space then @ then type then maybe space then { then maybe space then key then maybe space then ,
         self.regex_type_key = re.compile(r'\s?@(?P<type>(.*?))\s?{\s?(?P<key>(.*?))\s?,')
         # maybe space then field then maybe space then = then maybe space then content then maybe space then }
         # $ means: ends with
         self.regex_field_content = re.compile(r'\s?(?P<field>(.*?))\s?=\s?{\s?(?P<content>(.*?))\s?}$')
 
-    def parse(self, filename, append=False):
+    def parse(self, filename: str, append=False):
         assert os.path.isfile(filename)
         if not append:
             self.entries = []
@@ -59,7 +59,7 @@ class BibtexParser:
                 opening_brackets, closing_brackets = 0, 0
         file.close()
 
-    def write(self, filename, pretty_print=True, append=False):
+    def write(self, filename: str, pretty_print=True, append=False):
         with open(filename, 'a+' if append else 'w+') as file:
             for entry in self.entries:
                 output = ''
@@ -80,21 +80,46 @@ class BibtexParser:
                 output += '}\n\n'
                 file.write(output)
 
+    def append_entries(self, entries):
+        self.entries += entries
+
     def get_all_keys(self):
         return [entry.key for entry in self.entries]
 
-    def get_entry(self, key):
+    def get_entry(self, key: str):
         keys = self.get_all_keys()
         return self.entries[keys.index(key)] if key in keys else None
 
-    def set_order_of_fields(self, order, keys=None):
+    def get_entries_where_field_equals_content(self, field, content):
+        entries = []
+        for entry in self.entries:
+            if field in entry.fields:
+                if entry.contents[entry.fields.index(field)] == content:
+                    entries.append(entry)
+        return BibtexParser(entries)
+
+    def get_entries_in_year(self, year):
+        return self.get_entries_where_field_equals_content('year', str(year))
+
+    def get_entries_where_content_is_in_field(self, content, field):
+        entries = []
+        for entry in self.entries:
+            if field in entry.fields:
+                if content in entry.contents[entry.fields.index(field)]:
+                    entries.append(entry)
+        return BibtexParser(entries)
+
+    def get_entries_with_author(self, author):
+        return self.get_entries_where_content_is_in_field(author, 'author')
+
+    def set_order_of_fields(self, order: list, keys=None):
         if keys is None:
             keys = self.get_all_keys()
         for entry in self.entries:
             if entry.key in keys:
                 entry.set_order_of_fields(order)
 
-    def set_field_last(self, field):
+    def set_field_last(self, field: str):
         for entry in self.entries:
             entry.set_field_last(field)
 
